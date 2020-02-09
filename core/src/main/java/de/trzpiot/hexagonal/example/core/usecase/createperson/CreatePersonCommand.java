929a2c4c91be84c05da6c982e74b29b332419a62
Sebastian Gabriel Trzpiot <mail@trzpiot.de>
2020-02-09 16:53:01 +0100

    Make CreatePersonCommand immutable

--- core/src/main/java/de/trzpiot/hexagonal/example/core/usecase/createperson/CreatePersonCommand.java
+++ core/src/main/java/de/trzpiot/hexagonal/example/core/usecase/createperson/CreatePersonCommand.java
@@ -1,10 +1,11 @@
 package de.trzpiot.hexagonal.example.core.usecase.createperson;
 
-import lombok.Data;
+import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.Value;
 
-@Data
-@NoArgsConstructor
+@Value
+@NoArgsConstructor(force = true, access = AccessLevel.PRIVATE)
 public class CreatePersonCommand {
     private String name;
     private String firstName;
